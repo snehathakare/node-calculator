1f2b5da31994839b039df5cf7d82dd7c69f6f075
Sneha <snhthakare@gmail.com>
2022-01-21 17:01:33 +0530

    Adds logic for BMI calculator

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express")
+const bodyParser = require('body-parser')
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.listen(3000)
 
 app.get('/', function (req, res) {
-    res.send('Hello I am a calculator')
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/bmi', function (req, res) {
+    var weight = Number(req.body.weight)
+    var height = Number(req.body.height)
+    var result = weight / (height * height)
+    res.send("Your Body Mass Index is" + " " + result)
 })
